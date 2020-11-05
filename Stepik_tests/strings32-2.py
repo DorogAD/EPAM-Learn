b08101abe7ae0e1f2b4bdb3321ff1a3840f7d715
DorogAD <sa_do@tut.by>
2020-11-05 17:08:31 +0300

    first commit

---
+++ Stepik_tests/strings32-2.py
@@ -0,0 +1,28 @@
+'''
+Вашей программе на вход подаются две строки s и t, состоящие из строчных латинских букв.
+
+Выведите одно число – количество вхождений строки t в строку s.
+'''
+s = 'abababa'
+t = 'aba'
+
+start = 0
+end = start + len(t)
+end_len = len(s) + 1 - len(t)
+counter = 0
+print(start, end, end_len)
+if t not in s:
+    print(0)
+elif s == t:
+    print(1)
+else:
+    while start != end_len:
+        word = s[start:end]
+        if word == t:
+            counter = counter + 1
+        start = start + 1
+        end = end + 1
+            
+    print(counter)
+        
+
