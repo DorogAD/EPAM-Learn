e2c761b020d6c23db965095919826a737b247f0a
DorogAD <sa_do@tut.by>
2020-11-27 17:33:20 +0300

    solved task 3-2-11

---
+++ stepik_tasks/python_basics_and_usage/unit3.2/task3-2-11.py
@@ -0,0 +1,31 @@
+"""
+https://stepik.org/lesson/24470/step/11?unit=6776
+
+
+Вам дана последовательность строк.
+Выведите строки, содержащие слово, состоящее из двух одинаковых частей (тандемный повтор).
+
+Sample Input:
+
+blabla is a tandem repetition
+123123 is good too
+go go
+aaa
+
+Sample Output:
+
+blabla is a tandem repetition
+123123 is good too
+"""
+import re
+import sys
+
+output = []
+
+pattern = r'\b(\w+)\1\b'
+
+for line in sys.stdin:
+    line = line.rstrip()
+    result = re.findall(pattern, line)
+    if len(result) > 0:
+        print(line)
